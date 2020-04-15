dd2b9c3b3850a30fda3fa080c62550d727f7c7cf
ManEipAay39 <connectmanas39@gmail.com>
2020-04-16 03:40:56 +0530

    New Commit

---
+++ src/main/java/com/manas/launchManApp.java
@@ -0,0 +1,16 @@
+package com.manas;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class launchManApp {
+
+	public static void main(String[] args) {
+				System.setProperty("webdriver.gecko.driver","C:\\Users\\manas\\Desktop\\WD\\jar\\geckodriver.exe");
+				  WebDriver driver=new FirefoxDriver();
+				  driver.get("http://facebook.com");
+				  driver.manage().window().maximize();
+				  System.out.println("New ManashoreTech App");
+	}
+
+}
